6020077dd4ae315b0a87bfcc2ab105c563cbeda7
Oskari Virtaoja <oskari@smartly.io>
2019-12-17 11:53:46 +0200

    Configure git

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,14 @@ BUMP_VERSION=$1
 AUTH_TOKEN_STRING=$2
 REGISTRY=$3
 
+EMAIL=${EMAIL:-"bot@lerna-publish-action"}
+USERNAME=${USERNAME:-"lerna publish action bot"}
+
 echo $AUTH_TOKEN_STRING >> ~/.npmrc
+
+# Setup git
+
+git config user.email "$EMAIL"
+git config user.name "$USERNAME"
+
 node_modules/.bin/lerna publish --registry=$REGISTRY --yes $BUMP_VERSION
